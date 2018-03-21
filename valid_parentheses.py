4eea0d74f269b9c7e0dedb23ee0848373ffd6d69
Yuan Zhou <zhouyuan78@hotmail.com>
2018-03-21 10:22:26 -0700

    add files

---
+++ valid_parentheses.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+
+# use dict to represent parenthesis pairs
+def is_well_formed(s):
+    left_chars, lookup = [], {'(': ')', '{': '}', '[': ']'}
+    for c in s:
+        if c in lookup:
+            left_chars.append(c)
+        elif not left_chars or lookup[left_chars.pop()] != c:
+            # Unmatched right char or mismatched chars.
+            return False
+    return not left_chars  # left_chars could be empty or some left-side braces
+
+
+def small_test():
+    assert is_well_formed('()')
+    assert is_well_formed('()[]{}')
+    assert is_well_formed('[()[]]{}')
+    assert is_well_formed('(()[]{()[]{}{}})')
+    assert not is_well_formed('([)]')
+    assert not is_well_formed('[')
+    assert not is_well_formed('(()[]{()[]{})({}})')
+
+
+def main():
+    small_test()
+    if len(sys.argv) == 2:
+        print(is_well_formed(sys.argv[1]))
+
+
+if __name__ == '__main__':
+    main()
