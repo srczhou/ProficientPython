3d5842ee9e26778d96c9a714fc256bbf2ef2b7fe
Yuan Zhou <zhouyuan78@hotmail.com>
2018-03-26 22:25:06 -0700

    add files

---
+++ connect_leaves_binary_tree.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+class BinaryTreeNode:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+# use "+" to combine lists
+def create_list_of_leaves(tree):
+    if not tree:
+        return []
+    if not tree.left and not tree.right:
+        return [tree]
+    # First do the left subtree, and then do the right subtree.
+    return create_list_of_leaves(tree.left) + create_list_of_leaves(tree.right)
+
+
+def main():
+    #      3
+    #    2   5
+    #  1    4 6
+    tree = BinaryTreeNode(3)
+    tree.left = BinaryTreeNode(2)
+    L = create_list_of_leaves(tree)
+    assert len(L) == 1 and L[0].data == 2
+
+    tree.left.left = BinaryTreeNode(1)
+    tree.right = BinaryTreeNode(5)
+    tree.right.left = BinaryTreeNode(4)
+    tree.right.right = BinaryTreeNode(6)
+    L = create_list_of_leaves(tree)
+    output = []
+    # should output 1, 4, 6
+    for l in L:
+        output.append(l.data)
+        print(l.data)
+    golden_res = [1, 4, 6]
+    assert output == golden_res
+
+
+if __name__ == '__main__':
+    main()
