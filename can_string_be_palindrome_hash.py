7be567dbf8f11dde66894302a079c40b7ca5d615
Yuan Zhou <zhouyuan78@hotmail.com>
2018-04-06 20:59:00 -0700

    add 12.1

---
+++ can_string_be_palindrome_hash.py
@@ -0,0 +1,7 @@
+import collections
+
+
+def can_string_be_a_palindrome(s):
+    # A string can be permuted to form a palindrome if and only if the number
+    # of chars whose frequencies is odd is at most 1.
+    return sum(v % 2 for v in collections.Counter(s).values()) <= 1
